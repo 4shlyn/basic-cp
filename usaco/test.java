23426f71cd2b144d310bbd81ef84e3b3fd4d7f53
ashlyn <68078477+4shlyn@users.noreply.github.com>
2023-09-07 21:54:51 -0400

    Update test.java

--- usaco/test.java
+++ usaco/test.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.HashMap;
 
-class test {
+class milk{
     public static void main(String[] args) {
 
         HashMap<Integer, Integer> hierarchy = new HashMap<Integer, Integer>();
@@ -12,27 +12,37 @@ class test {
         int[] order = new int[cows+1];
         String[] lary = s.nextLine().split(" ");
 
+        // setting hierarchies starting w index 0
         for (int i = 0; i < Integer.valueOf(tests[1]); i++) {
-            hierarchy.put(Integer.valueOf(lary[i]), i);
+            hierarchy.put(Integer.valueOf(lary[i]),i);
         }
+        // adding in set positions
         for (int i = 0; i < specified; i++) {
             String[] temp = s.nextLine().split(" ");
-            order[Integer.valueOf(temp[1])] = Integer.valueOf(temp[0]);
+            // set cow temp[0] to position temp[1]-1 bcz of indexing
+            order[Integer.valueOf(temp[1])-1] = Integer.valueOf(temp[0]);
 
         }
         int empty = 0;
         int last = 0;
+        Boolean found = false;
         for (int i = 0; i < cows; i++) {
             if (order[i] != 0) {
                 if (hierarchy.keySet().contains(order[i])) {
-                    last = i;
                     if (hierarchy.get(order[i]) < empty) {
                         System.out.println(last + 1);
+                        found = true;
                     }
+                    else{last = i;}
                 }
             } 
-            else {empty++;}
+            else {
+                empty++;
         }
+    }
+    if (!found) {
+        System.out.println(last+2);
+    }
         s.close();
     }
 }
